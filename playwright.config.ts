0aa67c7ad8b3ce91886d6cf038014d604be993ce
Nathan G <73437724+ndg8743@users.noreply.github.com>
2025-12-30 21:49:21 -0500

    Fix ES module __dirname in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, devices } from '@playwright/test'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const extensionPath = path.join(__dirname, 'dist')
 
 export default defineConfig({
